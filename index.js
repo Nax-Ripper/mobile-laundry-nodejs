e664b3aa490543cbab24fd8e9148edd4fe48770f
Nax <narendran8155@gmail.com>
2023-04-13 21:21:02 +0800

    edit db connection

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const app = express();
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGO_URI);
+    const connect = await mongoose.connect(`${process.env.MONGO_URI/UserInfo}`);
     console.log(`MongoDB Connected :${connect.connection.host}`);
   } catch (error) {
 
