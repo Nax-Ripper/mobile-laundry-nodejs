862f09be8747aa4c6101b77e22f414ea4259f1b7
Nax <narendran8155@gmail.com>
2023-04-13 21:23:00 +0800

    changed monogdb url

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const app = express();
 
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(`${process.env.MONGO_URI/UserInfo}`);
+    const connect = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected :${connect.connection.host}`);
   } catch (error) {
 
